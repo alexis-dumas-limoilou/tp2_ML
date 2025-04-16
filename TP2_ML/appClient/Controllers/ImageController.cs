1f25fc13192f0b182062a7410d7c806f2a7c645e
Nicolas <2396634@cegeplimoilou.ca>
2025-04-16 17:38:59 -0400

    ajout appClient pour téléverser une image

---
+++ TP2_ML/appClient/Controllers/ImageController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using appClient.Models;
+namespace appClient.Controllers
+{
+    public class ImageController : Controller
+    {
+        // GET: ImageController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ImageController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Upload(IFormFile Image, string Title)
+        {
+            if (Image != null && Image.Length > 0)
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    Image.CopyTo(memoryStream);
+                    var imageData = memoryStream.ToArray();
+
+                    // Sauvegarder les données dans la base ou autre traitement
+                    var model = new Image
+                    {
+                        ImageData = imageData
+                    };
+
+                    // Exemple : sauvegarder dans une base de données
+                    // _dbContext.Images.Add(model);
+                    // _dbContext.SaveChanges();
+                }
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ TP2_ML/appClient/Models/Image.cs
@@ -0,0 +1,7 @@
+﻿namespace appClient.Models
+{
+    public class Image
+    {
+        public byte[] ImageData { get; set; }
+    }
+}
