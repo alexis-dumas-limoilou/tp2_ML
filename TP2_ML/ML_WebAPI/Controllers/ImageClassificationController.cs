ce8414acb13640bbd173e135c3bc1f1192b8ebb1
MISSING <alexis.dumas.gd@gmail.com>
2025-04-16 19:33:07 -0400

    First version Post image for prediction

---
+++ TP2_ML/ML_WebAPI/Controllers/ImageClassificationController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ML_WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ImageClassificationController : ControllerBase
+    {
+        [HttpPost()]
+        public IActionResult Post(IFormFile imageFile)
+        {
+            if (imageFile == null || imageFile.Length == 0)
+                return BadRequest("Image data is required.");
+
+            var supportedTypes = new[] { "image/jpeg", "image/png", "image/bmp" };
+            if (!supportedTypes.Contains(imageFile.ContentType.ToLower()))
+                return BadRequest("Unsupported file format.");
+
+            byte[] image;
+            using (var memoryStream = new MemoryStream())
+            {
+                imageFile.CopyTo(memoryStream);
+                image = memoryStream.ToArray();
+            }
+
+            var imageData = new MLImagesModel.ModelInput()
+            {
+                ImageSource = image
+            };
+
+            var result = MLImagesModel.Predict(imageData);
+
+            return Ok(result.PredictedLabel);
+        }
+    }
+}
